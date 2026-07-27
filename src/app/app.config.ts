83a45ed536fcd2a309f473f9eee925d79c5884c1
Moussa Ndiaye <moussan2003@gmail.com>
2026-07-27 18:53:10 +0000

    initialisation du projet

---
+++ src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
+  ]
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDD8Q9Mz72oWeIM97gWVdSAoosj1HTcHBw",
+    authDomain: "warate-b9a50.firebaseapp.com",
+    projectId: "warate-b9a50",
+    storageBucket: "warate-b9a50.firebasestorage.app",
+    messagingSenderId: "814559394266",
+    appId: "1:814559394266:web:4b5f1ea42a6a256e99e8e3",
+    measurementId: "G-0C7W296M05"
+  }
+};
